b347df8de3a11bdff80da44589f3b167f82f6dbd
osvaldocabral <osvaldo_cabral@outlook.com.br>
2021-06-15 22:34:07 -0300

    listando do firebase

--- app/src/main/java/io/osvaldocabral/appbarbearia/DataSingleton.java
+++ app/src/main/java/io/osvaldocabral/appbarbearia/DataSingleton.java
@@ -1,15 +1,40 @@
 package io.osvaldocabral.appbarbearia;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
 import java.util.ArrayList;
 
 public class DataSingleton {
 
 
     public ArrayList<Establishment> listEstablishment = new ArrayList<>();
+    FirebaseFirestore firestore;
 
 
     public DataSingleton() {
-        listEstablishment.add(new Establishment("NOSTRINKS CABELEREIRO", "Rua do Barbeiro, 1345", "(41) 9 8888-8888", "/storage/emulated/0/Android/data/io.osvaldocabral.validadordepresenca/files/Pictures/pic_202106132046382536392854497873818.jpg"));
+        firestore = FirebaseFirestore.getInstance();
+        firestore.collection("establishments").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if(task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        System.out.println(document);
+                    }
+                }
+                else {
+                    Log.w("DEURUIM", "Error getting documents.", task.getException());
+                }
+            }
+        });
+//        listEstablishment.add(new Establishment("NOSTRINKS CABELEREIRO", "Rua do Barbeiro, 1345", "(41) 9 8888-8888", "/storage/emulated/0/Android/data/io.osvaldocabral.validadordepresenca/files/Pictures/pic_202106132046382536392854497873818.jpg"));
     }
 
 
@@ -19,5 +44,6 @@ public class DataSingleton {
     public static DataSingleton getInstance() {
         return instance;
     }
+
 
 }
