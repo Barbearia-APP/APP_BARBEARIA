9206b05a505f0cc57e865decf9a3cf5dfca1a312
osvaldocabral <osvaldo_cabral@outlook.com.br>
2021-06-19 15:00:43 -0300

    trazendo dados do firebase

--- app/src/main/java/io/osvaldocabral/appbarbearia/DataSingleton.java
+++ app/src/main/java/io/osvaldocabral/appbarbearia/DataSingleton.java
@@ -5,37 +5,35 @@ import android.util.Log;
 import androidx.annotation.NonNull;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import org.w3c.dom.Document;
+
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class DataSingleton {
 
 
     public ArrayList<Establishment> listEstablishment = new ArrayList<>();
-    FirebaseFirestore firestore;
+    public FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+    public Task<QuerySnapshot> taskFirestore;
 
 
     public DataSingleton() {
-        firestore = FirebaseFirestore.getInstance();
-        firestore.collection("establishments").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                if(task.isSuccessful()) {
-                    for (QueryDocumentSnapshot document : task.getResult()) {
-                        System.out.println(document);
-                    }
-                }
-                else {
-                    Log.w("DEURUIM", "Error getting documents.", task.getException());
-                }
-            }
-        });
-//        listEstablishment.add(new Establishment("NOSTRINKS CABELEREIRO", "Rua do Barbeiro, 1345", "(41) 9 8888-8888", "/storage/emulated/0/Android/data/io.osvaldocabral.validadordepresenca/files/Pictures/pic_202106132046382536392854497873818.jpg"));
+        taskFirestore = firestore.collection("establishments").get();
     }
+
 
 
     public static DataSingleton instance = new DataSingleton();
--- app/src/main/java/io/osvaldocabral/appbarbearia/ListEstabelecimento.java
+++ app/src/main/java/io/osvaldocabral/appbarbearia/ListEstabelecimento.java
@@ -1,15 +1,24 @@
 package io.osvaldocabral.appbarbearia;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
 public class ListEstabelecimento extends AppCompatActivity {
 
     RecyclerView recyclerView;
     EstablishmentAdapter adapter;
+    Task<QuerySnapshot> querySnapshotTask;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +31,37 @@ public class ListEstabelecimento extends AppCompatActivity {
         recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        querySnapshotTask = DataSingleton.getInstance().taskFirestore;
+        querySnapshotTask.addOnCompleteListener(retrieveAndFillData());
+
     }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        adapter.notifyDataSetChanged();
+    }
+
+    private OnCompleteListener<QuerySnapshot> retrieveAndFillData() {
+        return new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                for (QueryDocumentSnapshot document : task.getResult()) {
+                    DataSingleton.getInstance().listEstablishment.add(
+                            new Establishment((String) document.getData().get("name"),
+                                    (String) document.getData().get("address"),
+                                    (String) document.getData().get("phone"),
+                                    "/storage/emulated/0/Android/data/io.osvaldocabral.validadordepresenca/files/Pictures/pic_202106132046382536392854497873818.jpg"
+                            )
+                    );
+
+                    adapter.notifyDataSetChanged();
+                }
+            }
+        };
+    }
+
+
 }
