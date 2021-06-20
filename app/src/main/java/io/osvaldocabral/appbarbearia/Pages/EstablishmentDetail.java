58983ac061f0f51d4ff616f4a69cced2a09261fe
osvaldocabral <osvaldo_cabral@outlook.com.br>
2021-06-20 11:19:03 -0300

    '

---
+++ app/src/main/java/io/osvaldocabral/appbarbearia/Pages/EstablishmentDetail.java
@@ -0,0 +1,42 @@
+package io.osvaldocabral.appbarbearia.Pages;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import io.osvaldocabral.appbarbearia.Model.Establishment;
+import io.osvaldocabral.appbarbearia.R;
+
+
+public class EstablishmentDetail extends AppCompatActivity {
+
+
+    Button buttonSave;
+    EditText editTextName, editTextAddress, editTextPhone;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_establishment_detail);
+
+        buttonSave = findViewById(R.id.buttonSave);
+        editTextName = findViewById(R.id.editTextName);
+        editTextAddress = findViewById(R.id.editTextAddress);
+        editTextPhone = findViewById(R.id.editTextPhone);
+    }
+
+
+    public void saveEstablishmentClicked(View view) {
+        String name = editTextName.getText().toString();
+        String address = editTextAddress.getText().toString();
+        String phone = editTextPhone.getText().toString();
+
+        new Establishment(name, address, phone, "");
+    }
+
+
+}
